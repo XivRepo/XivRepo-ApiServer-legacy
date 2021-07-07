241f604698313d648c607f15f0a48d0096cccad0
lejliu <14846866+lejliu@users.noreply.github.com>
2021-07-07 04:28:40 -0400

    Added migration for updating the dependencies table

---
+++ migrations/20210701694200_alter_dependencies.sql
@@ -0,0 +1,9 @@
+ALTER TABLE dependencies
+-- drop and add to clear the now obsolete version IDs?
+-- previously referenced versions instead of mods
+DROP COLUMN dependent_id,
+ADD COLUMN dependent_id bigint REFERENCES mods ON UPDATE CASCADE NOT NULL,
+DROP COLUMN dependency_id,
+ADD COLUMN dependency_id bigint REFERENCES mods ON UPDATE CASCADE NOT NULL,
+ADD COLUMN version_id bigint REFERENCES versions,
+ADD COLUMN min_version_num varchar(32)
