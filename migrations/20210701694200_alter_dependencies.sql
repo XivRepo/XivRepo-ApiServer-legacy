50d7028f004e2c769fddf590713851fbc9d43160
lejliu <14846866+lejliu@users.noreply.github.com>
2021-07-09 04:29:40 -0400

    Changed type of pkey in the dependencies table

--- migrations/20210701694200_alter_dependencies.sql
+++ migrations/20210701694200_alter_dependencies.sql
@@ -1,6 +1,7 @@
 ALTER TABLE dependencies
 -- drop and add to clear the now obsolete version IDs?
 -- previously referenced versions instead of mods
+ALTER COLUMN id TYPE bigint,
 DROP COLUMN dependent_id,
 ADD COLUMN dependent_id bigint REFERENCES mods ON UPDATE CASCADE NOT NULL,
 DROP COLUMN dependency_id,
